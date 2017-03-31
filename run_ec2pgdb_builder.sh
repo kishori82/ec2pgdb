359ce03b76a1e71e4aa323afb35c770ce0d13b4f
dongjae <kishori82@gmail.com>
2017-03-31 11:08:22 -0700

    updated the daemon script for init.d

--- run_ec2pgdb_builder.sh
+++ run_ec2pgdb_builder.sh
@@ -1,5 +1,20 @@
 #!/bin/bash 
 
+# Warn and exit if something is already listening on port 1555.
+if [ `netstat -a | fgrep 1555 | fgrep -c LISTEN` == 1 ]; then
+  echo Something is already listening on port 1555.
+  echo Please stop whatever it is, and then try $0 again.
+  exit
+fi
+# Start Xvfb as X-server display #1. If it's already been started, no harm done.
+# This works in conjunction with the pathway-tools script which needs to have:
+#   setenv DISPLAY localhost:1
+#Xvfb :1 > & /dev/null &
+Xvfb :99 & 
+export DISPLAY=:99
+# Start GNU screen (hit Ctrl-A Ctrl-D to detach). Within it, start Pathway Tools.
+# screen -m -d /home/aic-export/pathway-tools/ptools/[version]/pathway-tools -www -www-publish all  #(for version 10.0 or later)
+
 #sudo mv /filename /etc/init.d/
 #sudo chmod +x /etc/init.d/filename 
 #sudo update-rc.d filename defaults 
--- run_ec2pgdb_extractor.sh
+++ run_ec2pgdb_extractor.sh
@@ -1,9 +1,22 @@
 #!/bin/bash 
 
+# Warn and exit if something is already listening on port 1555.
+if [ `netstat -a | fgrep 1555 | fgrep -c LISTEN` == 1 ]; then
+  echo Something is already listening on port 1555.
+  echo Please stop whatever it is, and then try $0 again.
+  exit
+fi
+# Start Xvfb as X-server display #1. If it's already been started, no harm done.
+# This works in conjunction with the pathway-tools script which needs to have:
+#   setenv DISPLAY localhost:1
+#Xvfb :1 > & /dev/null &
+Xvfb :99 & 
+export DISPLAY=:99
+# Start GNU screen (hit Ctrl-A Ctrl-D to detach). Within it, start Pathway Tools.
+# screen -m -d /home/aic-export/pathway-tools/ptools/[version]/pathway-tools -www -www-publish all  #(for version 10.0 or later)
+
 #sudo mv /filename /etc/init.d/
 #sudo chmod +x /etc/init.d/filename 
 #sudo update-rc.d filename defaults 
-
-#python /home/ubuntu/ec2pgdb/ec2pgdb_builder.py  --role-type worker --worker_dir /home/ubuntu/worker_dir/ --key /home/ubuntu/.ssh/kishori.csail.csv --readyqueue ready_large
 
 python /home/ubuntu/ec2pgdb/ec2pgdb_builder.py --key ~/.ssh/kishori.konwar.csv --role-type worker  --process extract --readyqueue ready_extract --worker_dir ~/ec2pgdb/ --runningqueue running_extract --completequeue complete_extract
